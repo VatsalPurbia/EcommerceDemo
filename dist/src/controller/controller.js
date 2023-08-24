032dc1c4117a14414cdc64781c3b20bc7faf86cc
Vatsal <Vatsal Purbia>
2023-08-24 14:49:25 +0530

    error Resolve

--- dist/src/controller/controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProductControllerV1 = exports.userController = void 0;
-var userController_1 = require("./userController");
-Object.defineProperty(exports, "userController", { enumerable: true, get: function () { return userController_1.userController; } });
-var productController_1 = require("./productController");
-Object.defineProperty(exports, "ProductControllerV1", { enumerable: true, get: function () { return productController_1.ProductControllerV1; } });
