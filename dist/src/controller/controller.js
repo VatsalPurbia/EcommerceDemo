7cd1b0b7b9e7d1b8a5508094f590aa50ee663333
Vatsal <Vatsal Purbia>
2023-08-24 14:52:51 +0530

    error Fix

--- dist/src/controller/controller.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userController = void 0;
-var userController_1 = require("./userController");
-Object.defineProperty(exports, "userController", { enumerable: true, get: function () { return userController_1.userController; } });
